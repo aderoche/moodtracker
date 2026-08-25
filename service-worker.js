7197af7871b4d55707501460c708a4c099f49675
Allison DeRoche <Allison>
2026-08-25 15:27:14 -0400

    Debug service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,17 @@
-const CACHE_NAME = "mood-tracker-v2";
-
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json",
-    "./icon.png",
-    "./js/storage.js",
-    "./js/entries.js",
-    "./js/analytics.js",
-    "./js/charts.js"
-];
-
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(async cache => {
+                for (const file of FILES_TO_CACHE) {
+                    try {
+                        await cache.add(file);
+                        console.log("Cached:", file);
+                    } catch (error) {
+                        console.error("FAILED TO CACHE:", file, error);
+                    }
+                }
+            })
     );
 
     self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames
-                    .filter(name => name !== CACHE_NAME)
-                    .map(name => caches.delete(name))
-            );
-        })
-    );
-
-    self.clients.claim();
 });
